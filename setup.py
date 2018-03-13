9cb9a073343f89d7a0e5387c0169bd811fcf1e8f
Vineet <vineet@localhost.localdomain>
2018-03-13 08:14:26 -0700

    fifth commit

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(name = "escape_eschaton",
+      version = "0.1",
+      description = "Calculates most optimal path for escaping Eschaton Asteroid Field",
+      author='Vineet Joshi',
+      author_email='vineetjoshi006@gmail.com',
+      package_dir = {'lib': 'lib'},
+      test_suite="tests"
+    )
